d6a0470fd1b80b1c66c3a09a959e96ffd635712e
Dublin R <DublinRUserGroup@gmail.com>
2013-10-13 13:18:24 +0100

    Create GIS-CodeSegments1.R

---
+++ GIS-CodeSegments1.R
@@ -0,0 +1,17 @@
+# http://www.nyu.edu/projects/politicsdatalab/workshops/GISwR.pdf
+
+#################################################################
+
+# Basic tools: 7 Packages
+
+library(maps) #for creating geographical maps
+library(mapdata) #contains basic data for ’maps’
+library(maptools) #tools for handling spatial objects
+library(mapproj) #for creating projected maps
+library(raster) #tools to deal with raster maps
+library(ggplot2) #to create maps
+library(gpclib) #general polygon clipper
+
+##################################################################
+
+# requires: library(maps)
