9e0b5370233db0af89ab645ecb555346642c2c7b
Dublin R <DublinRUserGroup@gmail.com>
2013-11-18 15:43:19 +0000

    Update EuropeMaps.R

--- Rcode/EuropeMaps.R
+++ Rcode/EuropeMaps.R
@@ -14,3 +14,29 @@ plot(newmap)
 
 
 ##############################
+lat = c(40.702147,40.718217,40.711614);
+ 
+ 
+  lon = c(-74.012318,-74.015794,-73.998284);
+ 
+ 
+  center = c(mean(lat), mean(lon));
+ 
+ 
+  zoom <- min(MaxZoom(range(lat), range(lon)));
+ 
+ 
+  #this overhead is taken care of implicitly by GetMap.bbox(); 
+ 
+ 
+  markers = paste0("&markers=color:blue|label:S|40.702147,-74.015794&markers=color:",
+ 
+ 
+                   "green|label:G|40.711614,-74.012318&markers=color:red|color:red|",
+ 
+ 
+                   "label:C|40.718217,-73.998284")
+ 
+ 
+  MyMap <- GetMap(center=center, zoom=zoom,markers=markers,destfile = "MyTile1.png");
+ 
